e69d44da2beeb256599f2047dee3b0024f70a647
Anjali Singh <tanujanjali704@gmail.com>
2025-02-09 09:49:12 +0530

    color-picker

--- vit-react/src/App.jsx
+++ vit-react/src/App.jsx
@@ -3,108 +3,36 @@ import './App.css'
 
 
 function App() { 
+  const [backgroundColor, setBackgroundColor] = useState('fffff');
 
-  const [formData, setFormData] = useState({
-    text:'',
-    checkbox: false,
-    radio:'',
-    select:''
-  });
+  const colors = ['#ff0000','#008000','#0000FF','#FFC0CB','#58e8f9'];
 
-  const handleChange = (e)  => {
-    const {name, value, type, checked} = e.target;
-    setFormData({
-      ...formData,
-      [name]: type === 'checkbox' ? checked : value
-    });
-
-  };
- 
-  return (
-    <div className="form-container">
-      <h1>Form Example</h1>
-      <form>
-        {/* Text Input */}
-        <div className='form-field'>
-          <label>Text:</label>
-          <input
-                type='text'
-                name='text'
-                value={formData.text}
-                onChange={handleChange}>
-
-          </input>
-        </div>
-
-        {/* Checkbox */}
-        <div className='form-field'>
-          <label>
-            <input
-                type='checkbox'
-                name='checkbox'
-                checked={formData.checkbox}
-                onChange={handleChange}></input>
-              Checkbox
-          </label>
-
-
-          {/* Radio Buttons */}
-
-          <div className='form-field'>
-          <label>Radio:</label>
-
-          <label>
-          <input
-                type='radio'
-                name='radio'
-                value='option1'
-                checked={formData.radio === 'option1'}
-                onChange={handleChange}>
-          </input>
-          Option 1
-          </label>
-          <label>
-          <input
-                type='radio'
-                name='radio'
-                value='option2'
-                checked={formData.radio === 'option2'}
-                onChange={handleChange}>
-          </input>
-          Option 2
-          </label>
-        </div>
-
-
-        {/*Dropdown   */}
-
-        <div className='form-field'>
-          <label>Select:</label>
-          <select
-              name="select"
-              value={formData.select}
-              onChange={handleChange}>
-            <option value="">-- Choose an option --</option>
-            <option value="option1">Option 1</option>
-            <option value="option2">option 2</option>
-          </select>
-        </div>
-
-          <div className='form-data'>
-            <h3>Form Data</h3>
-            <p><strong>Text:</strong> {formData.text || 'N/A'}</p>
-            <p><strong>Checkbox:</strong> {formData.checkbox ? 'Checked' : 'Unchecked'}</p>
-            <p><strong>Radio:</strong> {formData.radio || 'N/A'}</p>
-            <p><strong>Select:</strong> {formData.select || 'N/A'}</p>
-
-          </div>
+  const handleColorChange = (color) => {
+    setBackgroundColor(color);
+  }
+return (
+  <div className='App' style={{backgroundColor}}>
+    <h1>Color Picker</h1>
+    <div className='color-palette'>
+      {colors.map((color, index) => (
+        <div 
+          key={index}
+          className='color-box'
+          style={{backgroundColor: color}}
+          onClick={() => handleColorChange(color)}
           
-        </div>
+        ></div>
+     ) )}
+    </div>
+    <div className='custom-color-picker'>
+      <input type='color'
+        value={backgroundColor}
+        onChange={(e) => handleColorChange(e.target.value)}
+      ></input>
 
-
-      </form>
-    </div> 
-  );
+    </div>
+    </div>
+);
 }
 
 
